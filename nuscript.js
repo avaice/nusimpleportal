82feb9912c4f6fb3e9cd15ca0da301fd8c92c155
avaice <dev@cho-ice.xyz>
2022-09-23 13:06:17 +0900

    log機能の調整

--- nuscript.js
+++ nuscript.js
@@ -1,13 +1,13 @@
-const LOG_ENABLED = true
+const LOG_ENABLED = false
 const SIMPLE_DAYVIEW = true
-const logoSrc = "https://portal.uprx.ce.nihon-u.ac.jp/uprx/images/univLogo.png"
+const logoSrc = 'https://portal.uprx.ce.nihon-u.ac.jp/uprx/images/univLogo.png'
 
-console.log("nusimpleportal is running!")
+printLog('nusimpleportal is running!')
 
 const observer = new MutationObserver((mutations) => {
   // ロゴの差し替え
   const headerLogo = document.querySelector(
-    "#headerForm > header > a > h1 > img"
+    '#headerForm > header > a > h1 > img'
   )
   if (headerLogo && headerLogo.src != logoSrc) {
     headerLogo.src = logoSrc
@@ -16,21 +16,18 @@ const observer = new MutationObserver((mutations) => {
   // 日表示のシンプル化
   ;[
     ...document.getElementsByClassName(
-      "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only inlineBlock"
+      'ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only inlineBlock'
     ),
-    ...document.querySelectorAll(".lessonMemoArea"),
-    document.querySelector("#portalSchedule1 > div.ourscheduleArea"),
-    document.querySelector("#portalSchedule1"),
+    ...document.querySelectorAll('.lessonMemoArea'),
+    document.querySelector('#portalSchedule1 > div.ourscheduleArea'),
+    document.querySelector('#portalSchedule1'),
   ].forEach((dom) => {
-    if (dom) dom.style.display = SIMPLE_DAYVIEW ? "none" : "block"
+    if (dom) dom.style.display = SIMPLE_DAYVIEW ? 'none' : 'block'
   })
 })
 observer.observe(document, { childList: true, subtree: true })
 
-window.onload = () => {
-  // Replaces header logo
-}
-
+// ログ表示が有効な場合はコンソールにログを出力する
 const printLog = (str) => {
   if (!LOG_ENABLED) return
   console.log(str)
